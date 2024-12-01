0f9d2c6cf7b78d94cc6026e90c2448cb3d0e3bb3
Anton Negodyaev <anton.negodyaev@vmedia.ca>
2024-12-02 00:00:46 +0300

    Set test MQTT credentials

--- include/defines/mqtt.h
+++ include/defines/mqtt.h
@@ -2,8 +2,8 @@
 
 #define MQTT_SERVER                     "192.168.13.200"
 #define MQTT_PORT                       1883
-#define MQTT_ID                         "mqtt_bath"
-#define MQTT_USER                       "bath"
+#define MQTT_ID                         "mqtt_bath_test"
+#define MQTT_USER                       "bath2"
 #define MQTT_PASS                       "0112358"
 #define MQTT_RECONNECT_TIMEOUT          10000
 #define MQTT_FLOOR_TEMPERATURE_TOPIC    "bath/floor_temp"
