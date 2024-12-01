be2f9c554e7aa059e660f3280475817a0ab3925d
Anton Negodyaev <anton.negodyaev@vmedia.ca>
2024-12-01 15:06:35 +0300

    Add local OTA, disable MQTT

---
+++ include/defines_ota.h
@@ -0,0 +1,9 @@
+#define OTA_INTERVAL            5000
+
+#define CURRENT_VERSION         "1.0"
+
+#define OTA_LOCAL_SERVER_HOST   "192.168.13.200"
+#define OTA_LOCAL_SERVER_PORT   443
+#define OTA_LOCAL_CONFIG_PATH   "firmwares/bath_floor/project.json"
+
+#define OTA_GITHUB_CONFIG_PATH  "modemaizer/bath_floor/master/project.json"
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,14 @@
 #include <AutoOTA.h>
 
 #include "defines.h"
+#include "defines_ota.h"
 #include "wifi.h"
 #include "mqtt.h"
 #include "http.h"
 #include "main_process.h"
 
-AutoOTA ota("1.4", "modemaizer/bath_floor/master/project.json");
+AutoOTA otaGithub(CURRENT_VERSION, OTA_GITHUB_CONFIG_PATH);
+AutoOTA otaLocal(CURRENT_VERSION, OTA_LOCAL_CONFIG_PATH, OTA_LOCAL_SERVER_HOST, OTA_LOCAL_SERVER_PORT);
 uint32_t otaPreviousMillis = 0;
 
 static void otaProcess() {
@@ -16,14 +18,23 @@ static void otaProcess() {
   if (currentMillis - otaPreviousMillis >= OTA_INTERVAL) {
     otaPreviousMillis = currentMillis;
     String ver, notes;
-    Serial.print("check updates, current version: ");
-    Serial.println(ota.version());
-    if (ota.checkUpdate(&ver, &notes)) {
+    Serial.print("current version (local OTA): ");
+    Serial.println(otaLocal.version());
+    Serial.print("current version (Github OTA): ");
+    Serial.println(otaGithub.version());
+    Serial.println("check updates local");
+    if (otaLocal.checkUpdate(&ver, &notes)) {
         Serial.println(ver);
         Serial.println(notes);
-        ota.updateNow();
+        otaGithub.updateNow();
     }
-    Serial.print("no any updates");
+    Serial.println("check updates on Github");
+    if (otaGithub.checkUpdate(&ver, &notes)) {
+        Serial.println(ver);
+        Serial.println(notes);
+        otaGithub.updateNow();
+    }
+    Serial.println("no any updates");
   }
   
 }
@@ -38,15 +49,15 @@ static void networkInit() {
   wifiInit();
   //mdnsInit();
   httpInit();
-  mqttInit();
-  Serial.println(ota.version());
+  //mqttInit();
+  Serial.println(otaGithub.version());
 }
 
 static void networkProcess() {
   wifiProcess();
   //MDNS.update();
   httpProcess();
-  mqttProcess();
+  //mqttProcess();
   otaProcess();
 }
 
