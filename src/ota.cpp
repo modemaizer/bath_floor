9bc55c7f9ebead79c59ec2e7bd2d431fe47a7133
Anton Negodyaev <anton.negodyaev@vmedia.ca>
2024-12-02 00:47:48 +0300

    Check update on timer

--- src/ota.cpp
+++ src/ota.cpp
@@ -2,34 +2,40 @@
 #include <AutoOTA.h>
 
 #include "defines/ota.h"
+#include "wifi.h"
 
 AutoOTA otaGithub(CURRENT_VERSION, OTA_GITHUB_CONFIG_PATH);
 AutoOTA otaLocal(CURRENT_VERSION, OTA_LOCAL_CONFIG_PATH);
-uint32_t otaPreviousMillis = 0;
+uint32_t ota_tmr = 0;
 
 void otaCheck() {
-  Serial.print("current version (local OTA): ");
-  Serial.println(otaLocal.version());
-  Serial.print("current version (Github OTA): ");
-  Serial.println(otaGithub.version());
-  Serial.println("checking updates");
-  String ver, notes;
-  if (otaLocal.checkUpdate(&ver, &notes)) {
-      Serial.print("found local: ");
-      Serial.println(ver);
-      Serial.println(notes);
-      otaLocal.update();
-  } else if (otaGithub.checkUpdate(&ver, &notes)) {
-        Serial.print("found on Github: ");
+  if (isWifiConnected() && (!ota_tmr || millis() - ota_tmr > OTA_INTERVAL)) {
+    ota_tmr = millis();
+    
+    Serial.print("current version (local OTA): ");
+    Serial.println(otaLocal.version());
+    Serial.print("current version (Github OTA): ");
+    Serial.println(otaGithub.version());
+    Serial.println("checking updates");
+    String ver, notes;
+    if (otaLocal.checkUpdate(&ver, &notes)) {
+        Serial.print("found local: ");
         Serial.println(ver);
         Serial.println(notes);
-        otaGithub.update();
-  } else {
-    Serial.println("no any updates");
+        otaLocal.update();
+    } else if (otaGithub.checkUpdate(&ver, &notes)) {
+          Serial.print("found on Github: ");
+          Serial.println(ver);
+          Serial.println(notes);
+          otaGithub.update();
+    } else {
+      Serial.println("no any updates");
+    }
   }
 }
 
 void otaProcess() {
+  otaCheck();
   if (!otaLocal.tick()) {
     otaGithub.tick();
   }
